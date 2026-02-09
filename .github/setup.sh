d92d5c396872fd455421bbd0e5771fd5143e8216
Daniel O'Connor <daniel.oconnor@gmail.com>
2026-02-10 05:54:47 +1030

    fix: Update setup script to install additional libraries (#16)
    Fix #15

--- .github/setup.sh
+++ .github/setup.sh
@@ -1,4 +1,4 @@
 python -m pip install torch torchvision
 python -m pip install -e ".[demo]"
 python -m pip install --upgrade streamlit
-sudo apt-get update && sudo apt-get install libgl1 -y
+sudo apt-get update && sudo apt-get install libgl1 libglib2.0-0 -y
